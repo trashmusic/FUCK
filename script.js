537d86574284591de85505a2f5c17d690b3257ae
trashmusic <zereliamid@gmail.com>
2018-03-10 16:31:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,224 @@
+var currentSong;
+var playing;
+var firstClick = true;
+var track = 0;
+var imageToggled;
+var timerUpdater;
+
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+
+function play(song, update) {
+    currentSong.pause();
+    currentSong.currentTime = 0;
+    currentSong = new Audio(song.audioPath);
+    currentSong.play();
+    if (firstClick) {
+        firstClick = false;
+    }
+
+    clearInterval(timerUpdater);
+    timerUpdater = setInterval(function() {
+        updateTime(currentSong)
+    }, 50);
+
+    $("#playIcon").text("pause")
+    firstClick = false;
+    playing = true;
+    track = songs.indexOf(song)
+
+    addSongInfo(song)
+
+    currentSong.onended = function() {
+        next();
+    }
+
+    if (!update) {
+        setAlbumArt(song.artPath);
+    }
+}
+
+function addSongInfo(song) {
+    $("#songinfo").html(
+        "<span class='song-name'>" + song.name +
+        "</span><br> <a target='_blank' href='" + song.link + "'>" +
+        song.artist + "</a>"
+    )
+
+    document.title = song.name +  " - " + song.artist
+
+    setBgArt(song.artPath);
+    changeFavicon(song.artPath);
+}
+
+function setBgArt(imgPath) {
+    //$("#bg-image").attr("src", imgPath);
+    if (!imageToggled) {
+        $("#new-bg").attr("src", imgPath);
+        $("#new-bg").css("opacity", .2);
+        $("#bg-image").css("opacity", 0);
+    } else {
+        $("#bg-image").attr("src", imgPath);
+        $("#bg-image").css("opacity", .2);
+        $("#new-bg").css("opacity", 0);        
+    }
+    imageToggled = !imageToggled;
+}
+
+$('body').keyup(function(e){
+   if(e.keyCode == 32 || e.keyCode == 75){
+       //space or k
+       e.preventDefault();
+       pause();
+   } else if (e.keyCode == 39) {
+       //right arrow
+        e.preventDefault();
+        next()
+   } else if (e.keyCode == 37) {
+       e.preventDefault();
+       prev();
+   } else if (e.keyCode == 74) {
+        //j
+        currentSong.currentTime -= 10;
+   } else if (e.keyCode == 76) {
+        //l
+        currentSong.currentTime += 10;
+   }
+});
+
+$('body').keydown(function(e){
+   if(e.keyCode == 32){
+       // doh ho ho
+       e.preventDefault();
+   }
+});
+
+function seek(e) {
+    var percent = e.offsetX / this.offsetWidth;
+    currentSong.currentTime = percent * currentSong.duration;
+}
+
+function pause() {
+    if (firstClick === true) {
+        play(songs[0]);
+        firstClick = false;
+        $("#playIcon").text("pause")
+    } else if (playing) {
+        currentSong.pause();
+        playing = false;
+        $("#playIcon").text("play_arrow")
+    } else {
+        currentSong.play();
+        playing = true;
+        $("#playIcon").text("pause")
+    }
+}
+
+function next() {
+    //loop if at the end
+    var toPlay;
+    var wasPlaying = playing;
+    if (track != songs.length-1) {
+        toPlay = songs[track + 1];
+    } else { toPlay = songs[0]; }
+    play(toPlay, true);
+    if (!wasPlaying) pause();
+    
+    $("#new-art").attr("src", toPlay.artPath);
+    $("#current-art").addClass("fadeLeft");
+    $("#new-art").addClass("fadeRight");
+    setTimeout(function() {
+        $("#current-art").removeClass("fadeLeft");
+        $("#new-art").removeClass("fadeRight");
+        $("#current-art").attr("src", toPlay.artPath);
+    }, 500)
+
+}
+
+function prev() {
+    var toPlay;
+    var wasPlaying = playing;
+    if (currentSong.currentTime > 3) { //rewinds if in the middle of a song
+        currentSong.currentTime = 0;
+        return;
+    } else if (track === 0) {
+            toPlay = (songs[songs.length-1]);
+    } else { toPlay = (songs[track - 1]); }
+    play(toPlay, true);
+    if (!wasPlaying) pause();
+
+
+    $("#new-art").attr("src", toPlay.artPath);
+    $("#current-art").addClass("fadeRight");
+    $("#new-art").addClass("fadeLeft");
+    setTimeout(function() {
+        $("#current-art").removeClass("fadeRight");
+        $("#new-art").removeClass("fadeLeft");
+        $("#current-art").attr("src", toPlay.artPath);
+    }, 500)
+}
+
+function updateTrackbar() {
+	var percent = (currentSong.currentTime / currentSong.duration) * 100
+	percent += "%"
+	$("#trackbar").css("width", percent)
+}
+
+function updateTime(song) {
+    $("#time").text(
+        formatTime(song.currentTime) + "/" + formatTime(song.duration)
+    );
+}
+
+$(document).ready(function() {
+    setInterval(updateTrackbar, 20)
+
+    currentSong = new Audio(songs[0].audioPath);
+
+    addSongInfo(songs[0]);
+    setAlbumArt(songs[0].artPath);
+    $("#current-art").attr("src", songs[0].artPath);
+
+    for (var i=0; i<songs.length; i++) {
+        $("#tracklist").append(
+            //a hack to get html entities from weird characters
+            "<p onclick=play(songs["+ i +"])>" + $("<div/>").text(songs[i].name).html() + "</p><br>"
+        )
+    }
+    
+    //$("#tracklist").append("<p><a href='songs.zip' target='_blank'> download all</a></p>")
+    
+    document.getElementById("trackbar-container").addEventListener("click", seek);
+})
+
+function setAlbumArt(path) {
+    $("#current-art").attr("src", path);
+}
+
+ function formatTime(seconds) {
+    minutes = Math.floor(seconds / 60);
+    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+  }
+
+
+
+function switchArt(newSong, direction) {
+    var artBlob = "<img src='"+newSong.artPath+"' id='newArt'>";
+    $("art-container").append(artBlob);
+}
+
+//overlay with absolute position, fade out with css and then update the original
---
+++ songs.js
@@ -0,0 +1,406 @@
+
+
+var a1 = {
+    audioPath : "mp3s/a day in the life.mp3",
+    artPath : "art/a day in the life.jpeg",
+    name : "A Day In The Life",
+    artist : "The Beatles",
+}
+
+var a2 = {
+    audioPath : "mp3s/Age of Adz.mp3",
+    artPath : "art/age of adz.jpeg",
+    name : "Age of Adz",
+    artist : "Sufjan Stevens",
+}
+
+var a3 = {
+    audioPath : "mp3s/Airbag.mp3",
+    artPath : "art/airbag.jpeg",
+    name : "Airbag",
+    artist : "Radiohead",
+}
+
+var a4 = {
+    audioPath : "mp3s/Archangel.mp3",
+    artPath : "art/archangel.jpg",
+    name : "Archangel",
+    artist : "Burial",
+}
+
+var a5 = {
+    audioPath : "mp3s/Beware.mp3",
+    artPath : "art/Beware.jpg",
+    name : "Beware",
+    artist : "Death Grips",
+}
+
+var a6 = {
+    audioPath : "mp3s/Black Skinhead.mp3",
+    artPath : "art/Black.jpg",
+    name : "Black Skinhead",
+    artist : "Kanye West",
+}
+
+var a7 = {
+    audioPath : "mp3s/Bronx Sniper.mp3",
+    artPath : "art/Bronx Sniper.jpg",
+    name : "Bronx Sniper",
+    artist : "Mister Heavenly",
+}
+
+var a8 = {
+    audioPath : "mp3s/Butcher Baker Candlestick Maker.mp3",
+    artPath : "art/Butcher.jpg",
+    name : "Butcher Baker Candlestick Maker",
+    artist : "Panda Bear",
+}
+
+var a9 = {
+    audioPath : "mp3s/Chicago.mp3",
+    artPath : "art/illinois.jpg",
+    name : "Chicago",
+    artist : "Sufjan Stevens",
+}
+
+var a10 = {
+    audioPath : "mp3s/Cicatriz ESP.mp3",
+    artPath : "art/Cicatriz.jpg",
+    name : "Cicatriz ESP",
+    artist : "The Mars Volta",
+}
+
+var a11 = {
+    audioPath : "mp3s/Come Together.mp3",
+    artPath : "art/Come Together.jpg",
+    name : "Come Together",
+    artist : "Spiritualized",
+}
+
+var a12 = {
+    audioPath : "mp3s/Cygnus....Vismund Cygnus.mp3",
+    artPath : "art/Cygnus.jpeg",
+    name : "Cygnus....Vismund Cygnus",
+    artist : "The Mars Volta",
+}
+
+var a13 = {
+    audioPath : "mp3s/Dreamcast.mp3",
+    artPath : "art/Dreamcast.jpg",
+    name : "Dreamcast",
+    artist : "Blank Banshee",
+}
+
+var a14 = {
+    audioPath : "mp3s/Drum n Bassa.mp3",
+    artPath : "art/Drum.jpeg",
+    name : "Drum n Bassa",
+    artist : "Infected Mushroom",
+}
+var a15 = {
+    audioPath : "mp3s/Epilogue.mp3",
+    artPath : "art/Epilogue.jpg",
+    name : "Epilogue",
+    artist : "The Antlers",
+}
+
+var a16 = {
+    audioPath : "mp3s/Fireworks.mp3",
+    artPath : "art/Fireworks.jpg",
+    name : "Fireworks",
+    artist : "Animal Collective",
+}
+
+var a17 = {
+    audioPath : "mp3s/First Day Of My Life.mp3",
+    artPath : "art/First.jpeg",
+    name : "First Day Of My Life",
+    artist : "Bright Eyes",
+}
+
+var a18 = {
+    audioPath : "mp3s/Funeralopolis.mp3",
+    artPath : "art/dope.jpeg",
+    name : "Funeralopolis",
+    artist : "Electric Wizard",
+}
+
+var a19 = {
+    audioPath : "mp3s/Gardenia.mp3",
+    artPath : "art/Gardenia.jpeg",
+    name : "Gardenia",
+    artist : "Kyuss",
+}
+
+var a20 = {
+    audioPath : "mp3s/Grace.mp3",
+    artPath : "art/Grace.jpeg",
+    name : "Grace",
+    artist : "Jeff Buckley",
+}
+
+var a21 = {
+    audioPath : "mp3s/Gummy.mp3",
+    artPath : "art/Gummy.jpeg",
+    name : "Gummy",
+    artist : "BROCKHAMPTON",
+}
+
+
+var a22 = {
+    audioPath : "mp3s/I Have Been Floated.mp3",
+    artPath : "art/Floated.jpeg",
+    name : "I Have Been Floated",
+    artist : "The Olivia Tremor Control",
+}
+
+var a23 = {
+    audioPath : "mp3s/I'll Keep Coming.mp3",
+    artPath : "art/Coming.jpeg",
+    name : "I'll Keep Coming",
+    artist : "Low Roar",
+}
+
+var a24 = {
+    audioPath : "mp3s/In The Aeroplane Over The Sea.mp3",
+    artPath : "art/Aeroplane.jpeg",
+    name : "In The Aeroplane Over The Sea",
+    artist : "Neutral Milk Hotel",
+}
+
+var a25 = {
+    audioPath : "mp3s/Jesus, Etc.mp3",
+    artPath : "art/Jesus.jpeg",
+    name : "Jesus, Etc.",
+    artist : "Wilco",
+}
+
+var a26 = {
+    audioPath : "mp3s/Kyoto.mp3",
+    artPath : "art/Kyoto.jpeg",
+    name : "Kyoto",
+    artist : "Yung Lean",
+}
+
+var a27 = {
+    audioPath : "mp3s/Let It Happen.mp3",
+    artPath : "art/Happen.jpeg",
+    name : "Let It Happen",
+    artist : "Tame Impala",
+}
+
+var a28 = {
+    audioPath : "mp3s/Maggot Brain.mp3",
+    artPath : "art/Maggot.jpeg",
+    name : "Maggot Brain",
+    artist : "Funkadelic",
+}
+var a29 = {
+    audioPath : "mp3s/My Girls.mp3",
+    artPath : "art/Merri.jpeg",
+    name : "My Girls",
+    artist : "Animal Collective",
+}
+
+var a30 = {
+    audioPath : "mp3s/Neighborhood #1.mp3",
+    artPath : "art/Neighborhood.jpeg",
+    name : "Neighborhood #1",
+    artist : "Arcade Fire",
+}
+
+var a31 = {
+    audioPath : "mp3s/Nude.mp3",
+    artPath : "art/Nude.jpg",
+    name : "Nude",
+    artist : "Radiohead",
+}
+
+var a32 = {
+    audioPath : "mp3s/Only Shallow.mp3",
+    artPath : "art/Shallow.jpeg",
+    name : "Only Shallow",
+    artist : "My Bloody Valentine",
+}
+
+var a33 = {
+    audioPath : "mp3s/Optimistic.mp3",
+    artPath : "art/kid.jpeg",
+    name : "Optimistic",
+    artist : "Radiohead",
+}
+
+var a34 = {
+    audioPath : "mp3s/Pangaea Girls.mp3",
+    artPath : "art/Pangaea.jpeg",
+    name : "Pangaea Girls",
+    artist : "Candy Claws",
+}
+
+var a35 = {
+    audioPath : "mp3s/PDA.mp3",
+    artPath : "art/PDA.jpeg",
+    name : "PDA",
+    artist : "Interpol",
+}
+
+var a36 = {
+    audioPath : "mp3s/Phoenix In Flight.mp3",
+    artPath : "art/Phoenix.jpeg",
+    name : "Phoenix In Flight",
+    artist : "Converge",
+}
+
+var a37 = {
+    audioPath : "mp3s/Power.mp3",
+    artPath : "art/Power.jpeg",
+    name : "Power",
+    artist : "Kanye West",
+}
+
+var a38 = {
+    audioPath : "mp3s/Pyramid Song.mp3",
+    artPath : "art/Pyramid.jpeg",
+    name : "Pyramid Song",
+    artist : "Radiohead",
+}
+
+var a39 = {
+    audioPath : "mp3s/Reverie.mp3",
+    artPath : "art/Reverie.jpeg",
+    name : "Reverie",
+    artist : "Arca",
+}
+
+var a40 = {
+    audioPath : "mp3s/Roads.mp3",
+    artPath : "art/Roads.jpeg",
+    name : "Roads",
+    artist : "Portishead",
+}
+
+var a41 = {
+    audioPath : "mp3s/Romantic Rights.mp3",
+    artPath : "art/Romantic.jpeg",
+    name : "Romantic Rights",
+    artist : "Death From Above 1979",
+}
+
+var a42 = {
+    audioPath : "mp3s/Sailing.mp3",
+    artPath : "art/Sailing.jpeg",
+    name : "Sailing",
+    artist : "Broken Whale",
+}
+var a43 = {
+    audioPath : "mp3s/Soma.mp3",
+    artPath : "art/Soma.jpeg",
+    name : "Soma",
+    artist : "The Strokes",
+}
+
+var a44 = {
+    audioPath : "mp3s/Soria Moria.mp3",
+    artPath : "art/crow.jpeg",
+    name : "Soria Moria",
+    artist : "Mount Eerie",
+}
+
+var a45 = {
+    audioPath : "mp3s/Sunbather.mp3",
+    artPath : "art/Sunbather.jpeg",
+    name : "Sunbather",
+    artist : "Deafheaven",
+}
+
+var a46 = {
+    audioPath : "mp3s/Swimming Pools.mp3",
+    artPath : "art/good kid.jpeg",
+    name : "Swimming Pools",
+    artist : "Kendrick Lamar",
+}
+
+var a47 = {
+    audioPath : "mp3s/Taro.mp3",
+    artPath : "art/awesome.jpeg",
+    name : "Taro",
+    artist : "Alt-J",
+}
+
+var a48 = {
+    audioPath : "mp3s/The Fever.mp3",
+    artPath : "art/Fever.jpeg",
+    name : "The Fever",
+    artist : "Death Grips",
+}
+
+var a49 = {
+    audioPath : "mp3s/The Glow pt 2.mp3",
+    artPath : "art/Glow.jpeg",
+    name : "The Glow Pt 2",
+    artist : "The Microphones",
+}
+
+
+var a50 = {
+    audioPath : "mp3s/The Man Who Sold The World.mp3",
+    artPath : "art/The Man Who Sold the World.jpeg",
+    name : "The Man Who Sold The World",
+    artist : "David Bowie",
+}
+
+var a51 = {
+    audioPath : "mp3s/The Suburbs.mp3",
+    artPath : "art/Suburbs.jpeg",
+    name : "The Suburbs",
+    artist : "Arcade Fire",
+}
+
+var a52 = {
+    audioPath : "mp3s/Vaporize.mp3",
+    artPath : "art/broken bells.jpeg",
+    name : "Vaporize",
+    artist : "Broken Bells",
+}
+
+var a53 = {
+    audioPath : "mp3s/Vietnam.mp3",
+    artPath : "art/Crystal Castles.jpeg",
+    name : "Vietnam",
+    artist : "Crystal Castles",
+}
+
+var a54 = {
+    audioPath : "mp3s/Washer.mp3",
+    artPath : "art/spiderland.jpeg",
+    name : "Washer",
+    artist : "Slint",
+}
+
+var a55 = {
+    audioPath : "mp3s/Wesley's Theory.mp3",
+    artPath : "art/pimp.jpeg",
+    name : "Wesley's Theory",
+    artist : "Kendrick Lamar",
+}
+
+var a56 = {
+    audioPath : "mp3s/Zebra.mp3",
+    artPath : "art/zebra.jpeg",
+    name : "Zebra",
+    artist : "Beach House",
+}
+
+
+var a57 = {
+    audioPath : "mp3s/Epitaph.mp3",
+    artPath : "art/heartonwave.jpg",
+    name : "Epitaph",
+    artist : "King Crimson",
+}
+
+
+var songs = [
+a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a57,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31,a32,a33,a34,a35,a36,a37,a38,a39,a40,a41,a42,a43,a44,a45,a46,a47,a48,a49,a50,a51,a52,a53,a54,a55,a56,
+
+]
